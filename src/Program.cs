a4c8a792e7cc62bacb4435f20ec77c91f7419f9e
Jean Carlo <jeancarlomo@hotmail.com>
2021-09-21 12:01:08 -0300

    feat: change API

--- src/Program.cs
+++ src/Program.cs
@@ -35,10 +35,32 @@ namespace Inoa
             
         }
 
-        private async Task<ObjectTest> MyFunction(string args){
+        private async Task<ObjectTest2> MyFunction(string args){
           string response = await client.GetStringAsync($"https://api.twelvedata.com/time_series?symbol={args}&interval=1min&apikey=ff4dfb66a51c4c77a59163cbb2adc5b6&source=docs");
-          return JsonConvert.DeserializeObject<ObjectTest>(response);
+        // string response2 = await client.GetStringAsync($"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=NN9O5HXEPOUGIT9H");
+      
+          var response3 = await client.GetStringAsync($"https://financialmodelingprep.com/api/v3/quote-short/{args}.SA?apikey={Environment.GetEnvironmentVariable("API_KEY")}");
+       //  System.Console.WriteLine(response3);
+        
+       List<ObjectTest2> list = JsonConvert.DeserializeObject<List<ObjectTest2>>(response3);
 
+       System.Console.WriteLine(list[0].price);
+        
+      return list[0];
+       // return JsonConvert.DeserializeObject<ObjectTest2>(response);
+          
+       // return JsonConvert.DeserializeObject<ObjectTest2>(response);
+         
+
+        }
+
+         public class ObjectTest2 {
+            [JsonProperty("symbol")]
+            public string symbol { get; set; }
+             [JsonProperty("price")]
+            public string price  { get; set; }
+             [JsonProperty("volume")]
+            public string volume { get; set; }
         }
 
         public class ObjectTest {
@@ -69,23 +91,26 @@ namespace Inoa
             
              Program program = new Program();
           var y = await program.MyFunction(args[0]);
-
-  
+          System.Console.WriteLine(y.price);
+          
+    double price;
+    double.TryParse(y.price, System.Globalization.NumberStyles.Any, CultureInfo.GetCultureInfo("en-US"), out price);
+      System.Console.WriteLine(price);
        double shouldSell;  
        double shouldBuy; 
        double.TryParse(args[1], System.Globalization.NumberStyles.Any, CultureInfo.GetCultureInfo("en-US"), out shouldSell); //adjust input according to US
        double.TryParse(args[2], System.Globalization.NumberStyles.Any, CultureInfo.GetCultureInfo("en-US"), out shouldBuy);
         
-         var  average = ((y.values[0].high+y.values[0].low)/2);
+      //    var  average = ((y.values[0].high+y.values[0].low)/2);
   //System.Console.WriteLine(average);
   //return;
-           if (average >shouldSell)
+           if (price >shouldSell)
            {
                Console.WriteLine("Enviar email de vender");
                await email.SendEmail(args[0], "sell");
            }
 
-            if (average < shouldBuy)
+            if (price < shouldBuy)
            {
                Console.WriteLine("Enviar email de comprar");
                await email.SendEmail(args[0], "buy");
