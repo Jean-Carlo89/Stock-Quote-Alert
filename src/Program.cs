205e42698a3b4cbed5d1da7ec9d5849aad4d4245
Jean Carlo <jeancarlomo@hotmail.com>
2021-09-21 18:41:46 -0300

    refactor: refactoring of some functions

--- src/Program.cs
+++ src/Program.cs
@@ -11,40 +11,37 @@ namespace Inoa
 {
     class Program
     {
-        //  public static Timer timer = new Timer(5000);//create setInterval
-
-      HttpClient client = new HttpClient();
-        
+     
+        HttpClient client = new HttpClient();
         static async Task Main(string[] args) {
+              
             DotNetEnv.Env.Load();
-           
-          //  timer.Elapsed += async (sender, e) => await MyElapsedMethod(sender, e,args); 
-          //   timer.AutoReset = true;
-          //   timer.Enabled = true;  
-          //   timer.Start();
-          //   Console.Read();
-        int delay;
-         int.TryParse(Environment.GetEnvironmentVariable("DELAY"), System.Globalization.NumberStyles.Any, CultureInfo.GetCultureInfo("en-US"), out delay);
-          
-         // System.Console.WriteLine(delay);
-        
-          while(true){
-
-            Console.WriteLine("Entrou no loop");
-           await MyElapsedMethod(args);
-           Console.WriteLine($"Enviou //  inico de espera {Environment.GetEnvironmentVariable("DELAY")} s");
-           await Task.Delay(TimeSpan.FromSeconds(delay));
-           Console.WriteLine($"Fim dos {delay} s");
-          }
             
+            int delay;
+            int.TryParse(Environment.GetEnvironmentVariable("DELAY"), System.Globalization.NumberStyles.Any, CultureInfo.GetCultureInfo("en-US"), out delay);
+              
+            try{
+             
+              do{
+                Console.WriteLine("Entrou no loop");
+                await MyElapsedMethod(args);
+                Console.WriteLine($"Enviou //  inico de espera {Environment.GetEnvironmentVariable("DELAY")} s");
+                await Task.Delay(TimeSpan.FromSeconds(delay));
+                Console.WriteLine($"Fim dos {delay} s");
+              }while(true);
+            
+            }catch(Exception ex){
+              System.Console.WriteLine(ex.Message);
+            }
+            
+                
         }
 
-        private async Task<ObjectTest2> MyFunction(string args){
-      
-        var response3 = await client.GetStringAsync($"https://financialmodelingprep.com/api/v3/quote-short/{args}.SA?apikey={Environment.GetEnvironmentVariable("API_KEY")}");
-      
-       List<ObjectTest2> list = JsonConvert.DeserializeObject<List<ObjectTest2>>(response3);
-       return list[0];
+        private async Task<ObjectTest2> GetStockInfo(string args){
+          var response3 = await client.GetStringAsync($"https://financialmodelingprep.com/api/v3/quote-short/{args}.SA?apikey={Environment.GetEnvironmentVariable("API_KEY")}");
+          System.Console.WriteLine(response3);
+          List<ObjectTest2> list = JsonConvert.DeserializeObject<List<ObjectTest2>>(response3);
+          return list[0];
         }
 
          public class ObjectTest2 {
@@ -56,60 +53,41 @@ namespace Inoa
             public string volume { get; set; }
         }
 
-        public class ObjectTest {
-            public object meta { get; set; }
-            public List<Values> values { get; set; }
-            public string status { get; set; }
-        }
-   
-         public class Values {
-            [JsonProperty("datetime")]
-            public string datetime { get; set; }
-            [JsonProperty("open")]
-            public double open { get; set; }
-            [JsonProperty("high")]
-            public double high { get; set; }
-           [JsonProperty("low")]
-             public double low { get; set; }
-            [JsonProperty("close")]
-             public double close { get; set; }
-            [JsonProperty("volume")]
-             public double volume { get; set; }
-        }
-
          private async static Task MyElapsedMethod(string[] args)
         {
 
-             var email = new EmailSender();
+          var email = new EmailSender();
             
-             Program program = new Program();
-          var y = await program.MyFunction(args[0]);
+          Program program = new Program();
+          var y = await program.GetStockInfo(args[0]);
           System.Console.WriteLine(y.price);
           
-    double price;
-    double.TryParse(y.price, System.Globalization.NumberStyles.Any, CultureInfo.GetCultureInfo("en-US"), out price);
-      System.Console.WriteLine(price);
-       double shouldSell;  
-       double shouldBuy; 
-       double.TryParse(args[1], System.Globalization.NumberStyles.Any, CultureInfo.GetCultureInfo("en-US"), out shouldSell); //adjust input according to US
-       double.TryParse(args[2], System.Globalization.NumberStyles.Any, CultureInfo.GetCultureInfo("en-US"), out shouldBuy);
+          
+          var price = ParseValue(y.price);
+          var shouldSell = ParseValue(args[1]);
+          var shouldBuy = ParseValue(args[2]);
         
-      //    var  average = ((y.values[0].high+y.values[0].low)/2);
-  //System.Console.WriteLine(average);
-  //return;
-           if (price >shouldSell)
-           {
-               Console.WriteLine("Enviar email de vender");
-               await email.SendEmail(args[0], "sell");
-           }
+          System.Console.WriteLine(price);
+        
+          if (price >shouldSell)
+          {
+              Console.WriteLine("Send 'sell' email");
+              await email.SendEmail(args[0], "sell");
+          }
 
             if (price < shouldBuy)
-           {
-               Console.WriteLine("Enviar email de comprar");
-               await email.SendEmail(args[0], "buy");
-           }
+          {
+              Console.WriteLine("Send 'buy' email");
+              await email.SendEmail(args[0], "buy");
+          }
 
          
+        }
+
+        static Double ParseValue(string value){
+           double parsedValue;
+           double.TryParse(value, System.Globalization.NumberStyles.Any, CultureInfo.GetCultureInfo("en-US"), out parsedValue); //adjust input according to US
+           return parsedValue;
         }
     }
 }
